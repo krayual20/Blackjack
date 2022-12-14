97097b9a087c3670af3270c777a05871c5ca02d7
krayual20 <krayual20@sps-prosek.cz>
2022-12-14 15:04:44 +0100

    14.12.2022

---
+++ Blackjack/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class Player
+    {
+        public string name { get; set; }
+        public int bitcoin { get; set; }
+        public int hand { get; set; }
+    }
+}
--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Blackjack;
+
+namespace OOP
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Player player = new Player();
+            player.name = "alex";
+
+            //STOP
+            Console.Write("click Enter, for close");
+            Console.ReadLine();
+
+        }
+    }
+}

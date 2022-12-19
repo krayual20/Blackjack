766fa64e93a23e58c63a576b325c82615b9630cf
krayual20 <krayual20@sps-prosek.cz>
2022-12-19 15:13:46 +0100

    19.12.2022

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -4,7 +4,6 @@ using System.Numerics;
 
 void InitGame()
 {
-
     var deck = new Deck();
     var dealer = new Dealer() { Name = "Dealer" };
 
@@ -30,9 +29,21 @@ void InitGame()
     game.StartRound(deck, dealer, new Player[] {player1});
 
 }
-
-InitGame();
-
-//STOP
-Console.Write("click Enter, for close");
-Console.ReadLine();
+while (true)
+{
+    Console.WriteLine("1-Play, 2-Rules, 3-Yarrow, 4-END");
+    string menu = Console.ReadLine();
+    if (menu == "1")
+    {
+        InitGame();
+    }else if(menu == "2")
+    {
+        Console.WriteLine("Rules. Blackjack hands are scored by their point total.\nThe hand with the highest total wins as long as it doesn't exceed 21;\na hand with a higher total than 21 is said to bust.\nCards 2 through 10 are worth their face value, and face cards (jack, queen, king)\nare also worth 10.");
+    }else if(menu == "3")
+    {
+        Console.WriteLine("Not working yet");
+    }else if(menu == "4")
+    {
+        System.Environment.Exit(1);
+    }
+}
